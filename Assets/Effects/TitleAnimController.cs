2ee811a670825af773d8616be55ea68c491882d5
g_koitan <tk_kakutasu@yahoo.co.jp>
2021-02-28 17:56:26 +0900

    かっこいいタイトルのアニメ(仮)

---
+++ Assets/Effects/TitleAnimController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleAnimController : MonoBehaviour
+{
+    [SerializeField]
+    TMProAnimator titleText;
+    [SerializeField]
+    float interval = 3f;
+    float t = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleText.StartAnimation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        if (t >= interval)
+        {
+            t -= interval;
+            titleText.StopAnimation();
+            titleText.StartAnimation();
+        }
+    }
+}
