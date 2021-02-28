0ac108c0859201dfe725009275c46d7c759b5187
g_koitan <tk_kakutasu@yahoo.co.jp>
2021-02-28 17:20:14 +0900

    ツイートボタン仮を作った

---
+++ Assets/Effects/TweetButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using naichilab;
+
+public class TweetButton : MonoBehaviour
+{
+    public void TweetWithHashtags()
+    {
+        UnityRoomTweet.Tweet("wanderlust", "「wanderlust」で" + PlayerPrefs.GetFloat("highscore", 0).ToString() + "mの旅をした！", "unityroom", "unity1week");
+    }
+}
