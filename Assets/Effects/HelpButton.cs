d0a96aa5d7fac5bba1776b1bee1738e60477b780
g_koitan <tk_kakutasu@yahoo.co.jp>
2021-02-28 11:47:51 +0900

    操作説明仮追加

---
+++ Assets/Effects/HelpButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class HelpButton : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform helpRect;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void HelpOpen()
+    {
+        helpRect.DOScale(Vector3.one, 0.5f).SetEase(Ease.OutBounce);
+    }
+
+    public void HelpClose()
+    {
+        helpRect.DOScale(Vector3.zero, 0.25f);
+    }
+}
