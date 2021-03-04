aba78fe585c90d03156bbfad9f5c0a4e008b20c9
小井田大賀 <tk_kakutasu@yahoo.co.jp>
2021-03-05 06:35:03 +0900

    IOSビルド用2

--- Assets/Effects/TweetButton.cs
+++ Assets/Effects/TweetButton.cs
@@ -7,6 +7,9 @@ public class TweetButton : MonoBehaviour
 {
     public void TweetWithHashtags()
     {
+#if UNITY_IOS
+#else
         UnityRoomTweet.Tweet("wanderlust", "「wanderlust ball」で" + PlayerPrefs.GetFloat("highscore", 0).ToString() + "mの旅をした！", "unityroom", "unity1week");
+#endif
     }
 }
